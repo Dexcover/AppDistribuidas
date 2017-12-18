50548ba24606d690daccf5c22bd1a07b33dbd400
Diego Chicaiza <diegochicaiza1@hotmail.com>
2017-12-18 18:23:43 -0500

    Initial1
    INITIAL COMMENT

--- app/src/main/java/softupcloud/appdistribuidas/MainActivity.java
+++ app/src/main/java/softupcloud/appdistribuidas/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //TODO LISTO PARA EMPEZAR
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
