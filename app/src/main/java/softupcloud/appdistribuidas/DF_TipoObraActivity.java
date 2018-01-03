5d4e8fe274fd311369b61f49ae270a7c496bf28a
daniel2835 <daniel2835@hotmail.com>
2018-01-03 17:58:09 -0500

    Realización de pantallas
    Pantallas realizadas

---
+++ app/src/main/java/softupcloud/appdistribuidas/DF_TipoObraActivity.java
@@ -0,0 +1,13 @@
+package softupcloud.appdistribuidas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DF_TipoObraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_df__tipo_obra);
+    }
+}
